f199dcbaa7950120e3225721ae56a0126053e702
Ezio <assassinste@gmail.com>
2019-01-26 18:20:29 +0800

    Get our first webpage 'running'

--- Chapter_01/mycafeshop/coffeeshop/settings.py
+++ Chapter_01/mycafeshop/coffeeshop/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#   BASE_DIR	    where the 'settings.py' lives at 
+#   PROJECT_DIR	    where the 'manage.py' lives at (CAN be renamed)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'coffeeshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['%s/templates/' % (PROJECT_DIR),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
